4df1bd344bc205c2de4c84943cebb980f206af38
BigRatMonster <bigratmonsterr@gmail.com>
2023-08-13 00:23:01 +0800

    4

---
+++ config.js
@@ -0,0 +1,96 @@
+var CONFIG = {
+  "maps": {
+    "world_isometric_day": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1691829094,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 9,
+      "name": "DPS Wall [2k2k] - Isometric",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "8b8t_2k2k_isometric_t1",
+      "tileSize": 384,
+      "world": "8b8t_2k2k",
+      "worldName": "8b8t_2k2k",
+      "worldSeaLevel": 64
+    },
+    "world_topdown_day": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1691829094,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 7,
+      "name": "2k2k Spawn - Topdown",
+      "renderView": "topdown",
+      "rotations": [
+        0
+      ],
+      "textureSize": 6,
+      "tileSetGroup": "8b8t_2k2k_topdown_t3",
+      "tileSize": 288,
+      "world": "8b8t_2k2k",
+      "worldName": "8b8t_2k2k",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "world_isometric_day",
+    "world_topdown_day"
+  ],
+  "tileSetGroups": {
+    "8b8t_2k2k_isometric_t1": {
+      "maxZoom": 9,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    },
+    "8b8t_2k2k_topdown_t3": {
+      "maxZoom": 7,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 3
+    }
+  }
+};
