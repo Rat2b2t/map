4888163a0453f75701399c0dc2817197481869d0
BigRatMonster <bigratmonsterr@gmail.com>
2024-01-27 23:50:08 +0800

    boom

---
+++ new/overviewerConfig.js
@@ -0,0 +1,66 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "World"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1706366678",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "North",
+            "zoomLevels": 9,
+            "defaultZoom": 1,
+            "maxZoom": 9,
+            "path": "north",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "World",
+            "last_rendertime": 1706276131,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -1868,
+                319,
+                161
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                -1868,
+                319,
+                161
+            ],
+            "north_direction": 0
+        }
+    ]
+};
